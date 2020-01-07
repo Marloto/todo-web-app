d6bcf348ae7c13853b124dbcf0fce966e15bfd75
Marloto <Marloto@users.noreply.github.com>
2020-01-07 01:12:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+console.log("Start ToDo-Web-App-Server");
 
 async function run() {
+    console.log("Connected to Database");
     try {
       // Load Model
       const ToDoItem = require('./models/item');
@@ -14,9 +16,13 @@ async function run() {
 }
 
 // Connect to Database
-mongoose.connect(
-  `mongodb://${process.env.MONGODB_USER || "user"}:${process.env.MONGODB_PASS || "pass"}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT || 27017}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`, 
+var url = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT || 27017}/${process.env.MONGODB_DATABASE}?authSource=admin&w=majority&readPreference=primary&retryWrites=true&ssl=false`;
+console.log("Connect to: " + url);
+console.log("Connect with: " + (process.env.MONGODB_USER || "user"));
+mongoose.connect(url, 
   {
+    user: process.env.MONGODB_USER || "user",
+    pass: process.env.MONGODB_PASS || "pass",
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: false
